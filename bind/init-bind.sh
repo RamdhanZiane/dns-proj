ac2315077c2ce68036ffe113192a9e1faa3de814
Ramdhan <issakalem@gmail.com>
2024-11-09 07:21:51 +0100

    b

--- bind/init-bind.sh
+++ bind/init-bind.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
+# Ensure proper permissions
+chown -R bind:bind /var/run/named /var/cache/bind /var/lib/bind /etc/bind
+chmod 755 /var/run/named /var/cache/bind /var/lib/bind /etc/bind
+
 # Create required files if they don't exist
 touch /etc/bind/zones.conf
+chown bind:bind /etc/bind/zones.conf
 
-# Start named in foreground
-exec /usr/sbin/named -g -c /etc/bind/named.conf -u bind
+# Start named in foreground with proper user
+exec /usr/sbin/named -g -u bind -c /etc/bind/named.conf
