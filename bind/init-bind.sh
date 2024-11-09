59b667decaa7ad2e953538184a0835017a2115bf
Ramdhan <issakalem@gmail.com>
2024-11-09 07:32:51 +0100

    s

--- bind/init-bind.sh
+++ bind/init-bind.sh
@@ -2,20 +2,10 @@
 # Create required files if they don't exist
 touch /etc/bind/zones.conf
 
-
-# Create required files if they don't exist
-chown bind:bind /var/run/named
-
-chmod 755 /var/run/named
-
-mkdir -p /var/run/named
-exec /usr/sbin/named -g -c /etc/bind/named.conf -u bind
-chmod 755 /var/run/named
-
-# Start named in foreground
-exec /usr/sbin/named -g -c /etc/bind/named.conf -u bind
-
-
-chown bind:bind /var/run/named
-# Start named in foreground
 # Ensure the session key directory exists
+mkdir -p /var/run/named
+chown bind:bind /var/run/named
+chmod 755 /var/run/named
+
+# Start named in foreground
+exec /usr/sbin/named -g -c /etc/bind/named.conf -u bind
