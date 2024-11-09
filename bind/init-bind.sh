ab50b1e37d740946765df3b988119d4953179d5b
Ramdhan <issakalem@gmail.com>
2024-11-09 21:32:27 +0100

    sadf

--- bind/init-bind.sh
+++ bind/init-bind.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Start named directly (already running as bind user)
-exec /usr/sbin/named -g -c /etc/bind/named.conf
+# Ensure proper permissions on startup
+chown -R bind:bind /var/cache/bind /var/run/named /var/lib/bind /etc/bind
+chmod 775 /var/cache/bind /var/run/named /var/lib/bind
+chmod 644 /etc/bind/*.conf
+
+# Start named as bind user
+exec su -s /bin/bash bind -c '/usr/sbin/named -g -c /etc/bind/named.conf'
